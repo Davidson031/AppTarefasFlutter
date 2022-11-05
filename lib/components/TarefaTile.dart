3961b4c35a4a89016ada14e199683de825f51fee
Davidson <davidson@hotmail.com>
2022-11-05 03:08:33 -0300

    Ajustando Tile de tarefas

--- lib/components/TarefaTile.dart
+++ lib/components/TarefaTile.dart
@@ -6,45 +6,49 @@ import 'package:flutter/src/widgets/framework.dart';
 
 class TarefaTile extends StatelessWidget {
   String titulo;
-  //const TarefaTile({Key? key}) : super(key: key);
 
   TarefaTile(this.titulo);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
-      height: 120,
+      margin: const EdgeInsets.only(left: 30, top: 20, right: 30),
+      padding: const EdgeInsets.only(top: 5, bottom: 5),
+      height: 100,
       decoration: const BoxDecoration(
         color: Color.fromRGBO(64, 75, 96, 0.8),
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
       child: Center(
         child: ListTile(
-          minLeadingWidth: 10,
-          minVerticalPadding: 0,
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 15.0,
-          ),
+          minLeadingWidth: 1,
           leading: Container(
-            height: 100,
-            width: 10,
+            padding: EdgeInsets.only(right: 10),
             decoration: BoxDecoration(
                 border: Border(
               right: BorderSide(width: 2.0, color: Colors.white),
             )),
+            child: Icon(Icons.done, color: Colors.green, size: 40.0),
           ),
           title: Text(
-            titulo,
-            style: TextStyle(color: Colors.white, fontSize: 25),
+            'blu blu blu blu blu',
+            style: TextStyle(color: Colors.white, fontSize: 20),
           ),
           subtitle: Row(
             children: [
-              Icon(Icons.linear_scale, color: Colors.grey, size: 50,),
+              Icon(
+                Icons.linear_scale,
+                color: Colors.grey,
+                size: 40,
+              ),
             ],
           ),
-          trailing: Icon(Icons.done, color: Colors.green, size: 50.0),
+          trailing: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.delete, color: Colors.red, size: 22.0),
+            ],
+          ),
         ),
       ),
     );
